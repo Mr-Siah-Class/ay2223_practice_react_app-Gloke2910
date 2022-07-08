a4228e19d1d737add2dbe06172ad3d65b34d12d8
Gloke2910 <gabrielloke2042@gmail.com>
2022-07-08 15:06:38 +0800

    Passing the URL Parameter to Single Product Screen #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import NavBar from './NavBar';
+import {
+  Route,
+  useParams,
+  Routes,
+  BrowserRouter
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomeScreen from './screens/Home';
+import NoScreen from './screens/NoScreen';
+import SingleProductcreen from './screens/SingleProductScreen';
+import ProductScreen from './screens/ProductScreen';
+import FooterBar from './FooterBar';
+
+const ProductIdWrapper = () => {
+  const { productId } = useParams();
+  return <SingleProductcreen productId={productId} />;
+};
+
+class App extends React.Component
+{
+    constructor() {
+        super()
+    }
+
+    render()
+    {
+        return (
+            <div>
+              <NavBar/>
+              <BrowserRouter>
+                  <Routes>
+                    <Route exact path="/" element={<HomeScreen/>}/>
+                    <Route path="/products" element={<ProductScreen/>}/>
+                    <Route path="/products/:productId" element={<ProductIdWrapper/>}/>
+                    <Route path="*" element={<NoScreen/>}/>
+                  </Routes>
+                </BrowserRouter>
+                
+                <FooterBar/>
+            </div>
+        );
+    }
 }
 
 export default App;
